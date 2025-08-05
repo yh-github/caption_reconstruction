61ee2ffde8dd1d0f4e5a743b1404ffd32c95a4d1
yh-github <yoav.haimovitch@post.runi.ac.il>
2025-08-05 21:44:27 +0300

    chore: disk_cache

--- src/run_experiments.py
+++ src/run_experiments.py
@@ -98,9 +98,9 @@ def build_experiments(config):
     evaluator.calc_idf(sents=data_loader.load_all_sentences())
 
     cache = diskcache.Cache(
-        directory=config['paths']['disk_cache'],
-        disk_min_file_size=1024,  # Compress anything over 1KB
-        disk_compress_level=1  # Use the fastest compression level
+        directory=config['paths']['disk_cache']
+        # ,disk_min_file_size=1024,  # Compress anything over 1KB
+        # disk_compress_level=1  # Use the fastest compression level
     ) # FIXME create manager and close()
     rs_builder = ReconstructionStrategyBuilder(cache)
     for strategy_params in config.get("recon_strategy", []):
