8b6d1de387e790513f6d5783a57a5a7d0c522b03
yh-github <yoav.haimovitch@post.runi.ac.il>
2025-09-07 16:50:50 +0300

    refactor: recon_strategy output, dataloaders

--- src/vectors/VectorRunner.py
+++ src/vectors/VectorRunner.py
@@ -1,56 +1,13 @@
 import logging
-from abc import abstractmethod, ABC
 from typing import Any
-from typing import Iterator
 import numpy as np
-from numpy.typing import NDArray
 from pathlib import Path
-
-from data_loaders import BaseDataLoader
-from embedder import Embedder
 from evaluation import metrics_to_json
+from vectors.dataloaders import VectorDataLoader
 from vectors.eval_vectors import VectorReconstructionEvaluator
-
-NPY_FILE_PATTERN = "*.npy"
 
 from masking import MaskingStrategy
 from vectors.reconstruction_startegies import VectorReconstructionStrategy
-
-class VectorDataLoader(ABC):
-    @abstractmethod
-    def load(self) -> Iterator[tuple[NDArray[np.float64], str]]:
-        pass
-
-class VectorFileLoader(VectorDataLoader):
-    def __init__(self, directory: Path, file_pattern: str = NPY_FILE_PATTERN):
-        self.directory = directory
-        self.file_pattern = file_pattern
-
-    @staticmethod
-    def find_numpy_files(directory: Path, file_pattern: str = NPY_FILE_PATTERN) -> list[Path]:
-        return list(directory.rglob(file_pattern))
-
-    @staticmethod
-    def load_numpy_files(
-        npy_files: list[Path],
-        max_rows: int | None = None
-    ) -> Iterator[tuple[NDArray[np.float64], str]]:
-        max_rows = max_rows or 60
-        for file_path in npy_files:
-            yield np.load(file_path)[:max_rows], file_path.stem
-
-    def load(self) -> Iterator[tuple[NDArray[np.float64], str]]:
-        yield from self.load_numpy_files(self.find_numpy_files(self.directory, self.file_pattern))
-
-class VectorConvertorLoader(VectorDataLoader):
-    def __init__(self, base_dataloader:BaseDataLoader, embedder:Embedder):
-        self.base_dataloader:BaseDataLoader=base_dataloader
-        self.embedder:Embedder=embedder
-
-    def load(self) -> Iterator[tuple[NDArray[np.float64], str]]:
-        for x in self.base_dataloader.load():
-            yield np.array(self.embedder.get_embeddings(x.video_id, x.get_texts()), dtype=np.float64), x.video_id
-
 
 class VectorRunner:
     """
@@ -64,15 +21,15 @@ class VectorRunner:
         masking_strategy: MaskingStrategy,
         reconstruction_strategy: VectorReconstructionStrategy,
         evaluator: VectorReconstructionEvaluator,
-        result_path: Path,
+        save_path: Path,
         conf_for_log:dict[str, Any]
     ):
         self.run_name = run_name
         self._data_loader = data_loader
         self._masking_strategy = masking_strategy
         self._reconstruction_strategy = reconstruction_strategy
-        self._evaluator = evaluator
-        self._result_path = result_path/run_name
+        self.evaluator = evaluator
+        self._result_path = save_path/run_name
         self.conf_for_log = conf_for_log
 
     def run(self) -> list[dict]:
@@ -92,12 +49,13 @@ class VectorRunner:
                 logging.warning(f'Bad indices found in reconstructed_video {video_id}, {masked_indices=}, skipping')
                 continue
 
-            video_metrics = self._evaluator.evaluate(reconstructed_vectors, m[masked_indices])
+            video_metrics = self.evaluator.evaluate(reconstructed_vectors, m[masked_indices])
 
             video_metrics.update({
                 "video_id": video_id,
                 "num_captions": len(m),
-                "masked": list(masked_indices)
+                "masked": list(masked_indices),
+                "recon_strategy": str(self._reconstruction_strategy)
             })
 
             all_metrics.append(video_metrics)
---
+++ src/vectors/dataloaders.py
@@ -0,0 +1,63 @@
+from abc import abstractmethod, ABC
+from pathlib import Path
+from typing import Iterator, Self
+import numpy as np
+from numpy.typing import NDArray
+from data_loaders import BaseDataLoader, get_data_loader
+from embedder import Embedder
+
+class VectorDataLoader(ABC):
+    @abstractmethod
+    def load(self) -> Iterator[tuple[NDArray[np.float64], str]]:
+        pass
+
+    @staticmethod
+    def from_config(data_config: dict):
+        """
+        Factory function that reads the config and returns the appropriate
+        data loader instance.
+        """
+        dataset_name = data_config.get("name")  # TODO rename to dataset_type?
+        data_path = data_config.get("path")
+        limit = data_config.get("limit")
+
+        if not dataset_name or not data_path:
+            raise ValueError("Dataset 'name' and 'path' must be specified in the config.")
+
+        if dataset_name == "np_files":
+            return VectorFileLoader(data_path, limit)
+        else:
+            return VectorConvertorLoader(get_data_loader(data_config), Embedder())
+
+
+class VectorFileLoader(VectorDataLoader):
+    def __init__(self, directory: Path, limit:int|None=None, file_pattern: str = "*.npy"):
+        self.directory = directory
+        self.limit = limit
+        self.file_pattern = file_pattern
+
+    def find_numpy_files(self) -> list[Path]:
+        files = sorted(list(self.directory.rglob(self.file_pattern)))
+        return files[:self.limit] if self.limit else files
+
+    @staticmethod
+    def load_numpy_files(
+        npy_files: list[Path],
+        max_rows: int | None = None
+    ) -> Iterator[tuple[NDArray[np.float64], str]]:
+        max_rows = max_rows or 60
+        for file_path in npy_files:
+            yield np.load(file_path)[:max_rows], file_path.stem
+
+    def load(self) -> Iterator[tuple[NDArray[np.float64], str]]:
+        yield from self.load_numpy_files(self.find_numpy_files())
+
+
+class VectorConvertorLoader(VectorDataLoader):
+    def __init__(self, base_dataloader: BaseDataLoader, embedder: Embedder):
+        self.base_dataloader: BaseDataLoader = base_dataloader
+        self.embedder: Embedder = embedder
+
+    def load(self) -> Iterator[tuple[NDArray[np.float64], str]]:
+        for x in self.base_dataloader.load():
+            yield np.array(self.embedder.get_embeddings(x.video_id, x.get_texts()), dtype=np.float64), x.video_id
